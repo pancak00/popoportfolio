67a0121a7a9d21ffb398665a0dff746451547427
unknown <you@example.com>
2026-06-24 20:27:21 +0800

    Pin Vite to v7 to resolve compatibility issue with TanStack Start and Vercel

--- dist/server/server.js
+++ dist/server/server.js
@@ -57,7 +57,7 @@ function renderErrorPage() {
 //#region src/server.ts
 var serverEntryPromise;
 async function getServerEntry() {
-	if (!serverEntryPromise) serverEntryPromise = import("./assets/server-BMCeIKZK.js").then((m) => m.default ?? m);
+	if (!serverEntryPromise) serverEntryPromise = import("./assets/server-Bw95_GTi.js").then((m) => m.default ?? m);
 	return serverEntryPromise;
 }
 async function normalizeCatastrophicSsrResponse(response) {
